d05fdbc4933da14eda57d7d463f628fa96dd8f5b
sreepathysois <54054574+sreepathysois@users.noreply.github.com>
2024-07-23 17:04:47 +0530

    Update index.php

--- mompopcafe/index.php
+++ mompopcafe/index.php
@@ -10,7 +10,7 @@
 
 	<div id="header" class="mainHeader">
 		<hr>
-		<div class="center">Staging App Test Mom &amp; Pop Caf&eacute;</div>
+		<div class="center">Prod App Test Mom &amp; Pop Caf&eacute;</div>
 	</div>
 	<br>
 	<?php
